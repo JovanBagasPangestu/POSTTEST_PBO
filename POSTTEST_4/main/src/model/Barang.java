43f845141812bcc5d11146c4bbed70c3ba764ee8
JovanBagasPangestu <pjovanbagas@gmail.com>
2026-04-11 22:42:30 +0800

    pt 4 done

---
+++ POSTTEST_4/main/src/model/Barang.java
@@ -0,0 +1,43 @@
+package model;
+
+public class Barang {
+    protected int id;
+    protected String namaBarang;
+    protected String deskripsi;
+
+    public Barang(int id, String namaBarang, String deskripsi) {
+        this.id = id;
+        this.namaBarang = namaBarang;
+        this.deskripsi = deskripsi;
+    }
+
+    public Barang() {}
+
+    public int getId() { return id; }
+    public String getNamaBarang() { return namaBarang; }
+    public String getDeskripsi() { return deskripsi; }
+
+    public void setId(int id) { this.id = id; }
+    public void setNamaBarang(String namaBarang) { this.namaBarang = namaBarang; }
+    public void setDeskripsi(String deskripsi) { this.deskripsi = deskripsi; }
+
+    public void tampilkanInfo() {
+        System.out.println("ID: " + id +
+                ", Nama: " + namaBarang +
+                ", Deskripsi: " + deskripsi);
+    }
+
+    // OVERLOADING
+    public void tampilkanInfo(boolean detail) {
+        if (detail) {
+            System.out.println("ID: " + id +
+                    ", Nama: " + namaBarang +
+                    ", Deskripsi: " + deskripsi);
+        } else {
+            System.out.println("Nama: " + namaBarang);
+        }
+    }
+
+    public void tampilkanInfo(String prefix) {
+        System.out.println(prefix + " " + namaBarang);}
+}
---
+++ POSTTEST_4/main/src/model/BarangDitemukan.java
@@ -0,0 +1,54 @@
+package model;
+
+public class BarangDitemukan extends Barang {
+    private String lokasiDitemukan;
+    private String tanggalDitemukan;
+    private String kontakPenemu;
+    private boolean sudahDikembalikan;
+
+    protected String kategori;
+    String kondisi;
+
+    public BarangDitemukan(int id, String namaBarang, String deskripsi,
+                           String lokasiDitemukan, String tanggalDitemukan, String kontakPenemu) {
+
+        super(id, namaBarang, deskripsi); // ambil dari parent
+        this.lokasiDitemukan = lokasiDitemukan;
+        this.tanggalDitemukan = tanggalDitemukan;
+        this.kontakPenemu = kontakPenemu;
+        this.sudahDikembalikan = false;
+    }
+
+    // Constructor default
+    public BarangDitemukan() {
+        super(0, "Barang Default", "Deskripsi Default");
+        this.lokasiDitemukan = "Lokasi Default";
+        this.tanggalDitemukan = "YYYY-MM-DD";
+        this.kontakPenemu = "Kontak Default";
+        this.sudahDikembalikan = false;
+    }
+
+    // Getter
+    public String getLokasiDitemukan() { return lokasiDitemukan; }
+    public String getTanggalDitemukan() { return tanggalDitemukan; }
+    public String getKontakPenemu() { return kontakPenemu; }
+    public boolean isSudahDikembalikan() { return sudahDikembalikan; }
+
+    // Setter
+    public void setLokasiDitemukan(String lokasiDitemukan) { this.lokasiDitemukan = lokasiDitemukan; }
+    public void setTanggalDitemukan(String tanggalDitemukan) { this.tanggalDitemukan = tanggalDitemukan; }
+    public void setKontakPenemu(String kontakPenemu) { this.kontakPenemu = kontakPenemu; }
+    public void setSudahDikembalikan(boolean sudahDikembalikan) { this.sudahDikembalikan = sudahDikembalikan; }
+
+    @Override
+    public void tampilkanInfo() {
+        System.out.println("=== Barang Ditemukan ===");
+        super.tampilkanInfo();
+
+        String status = sudahDikembalikan ? "Dikembalikan" : "Belum Dikembalikan";
+
+        System.out.println("Lokasi: " + lokasiDitemukan +
+                ", Tanggal: " + tanggalDitemukan +
+                ", Status: " + status);
+    }
+}
